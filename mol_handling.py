91a6c4af5610f8fa6f246da0bd3cdb84b9ecd581
ilisien <ian8lisien@gmail.com>
2025-02-19 22:20:25 -0500

    synonyms for iupac names

--- mol_handling.py
+++ mol_handling.py
@@ -4,13 +4,15 @@ from rdkit.Chem.inchi import MolToInchi, MolFromInchi
 from rdkit.Chem import AllChem
 import pubchempy as pcp
 
-def smiles_to_iupac(smiles):
+def smiles_to_iupac(smiles,synonyms_amount=3):
     """Convert a SMILES string to an IUPAC name."""
     mol = Chem.MolFromSmiles(smiles)
     inchi = MolToInchi(mol)
     compound = pcp.get_compounds(inchi, 'inchi')
     iupac_name = compound[0].iupac_name if compound else "Not found"
-    return iupac_name
+    compound[0].synonyms.remove(iupac_name)
+    synonyms = compound[0].synonyms[:synonyms_amount] if compound else "Not found"
+    return iupac_name, synonyms
 
 def iupac_to_smiles(iupac_name):
     """Convert an IUPAC name to a SMILES string."""
@@ -23,13 +25,14 @@ def draw_molecule(smiles):
     return Draw.MolToImage(mol)
 
 if __name__ == "__main__":
-    smiles = "CO"
+    smiles = "CCC=O"
     print(f"SMILES: {smiles}")
 
-    iupac_name = smiles_to_iupac(smiles)
+    iupac_name, synonyms = smiles_to_iupac(smiles,5)
     print(f"IUPAC Name: {iupac_name}")
+    print(f"Synonyms: {synonyms}")
 
     new_smiles = iupac_to_smiles(iupac_name)
     print(f"Reconstructed SMILES: {new_smiles}")
 
-    draw_molecule("C1=CC=C(C=C1)CCO").show()
+    draw_molecule(new_smiles).show()
