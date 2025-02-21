1d0de61dccf305a41115830cb0623b9a4e6043c7
ilisien <ian8lisien@gmail.com>
2025-02-21 08:29:38 -0500

    not quite working procedural pages

--- server.py
+++ server.py
@@ -3,11 +3,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 # Default value for the template
-value = "Hello, World!"
+card_titles = [["number1","number2","number3","number4","number5","number6","number7","number8","number9"],["number10","number11","number12","number13"]]
+filenumbers = [[1,2,3,4,5,6,7,8,9],[10,11,12,13]]
 
 @app.route('/')
 def index():
-    return render_template('flashcards.html', value=value)
+    return render_template('flashcards.html', card_titles=card_titles,filenumbers=filenumbers)
 
 if __name__ == "__main__":
     app.run(debug=True)
