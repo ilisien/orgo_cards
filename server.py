7f7a2f78ffd8a7f1a9dfa1b091bcc1ac305941d8
ilisien <ian8lisien@gmail.com>
2025-02-22 13:08:40 -0500

    change system for holding images, etc.

--- server.py
+++ server.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, request
-from utilities import mirror_backs, generate_pagelists
+from utilities import mirror_backs, generate_pagelists, test_filesystem
 
 app = Flask(__name__)
 
 # Default value for the template
 raw_titles = ["number1","number2","number3","number4","number5","number6","number7","number8","number9","number10","number11","number12","number13"]
 
-card_titles, filenumbers = generate_pagelists(raw_titles)
+card_titles, fronts, backs = generate_pagelists(raw_titles)
 
-print(card_titles, filenumbers)
+print(card_titles, fronts, backs)
 
 @app.route('/')
 def index():
-    return render_template('flashcards.html', card_titles=card_titles,filenumbers=filenumbers)
+    return render_template('flashcards.html', card_titles=card_titles,fronts=fronts,backs=backs)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- utilities.py
+++ utilities.py
@@ -1,3 +1,37 @@
+import requests, os
+from PIL import Image
+from io import BytesIO
+
+def get_placeholder(width=250, height=350):
+    """Downloads an image from Picsum and saves it to a file."""
+    url = f"https://picsum.photos/{width}/{height}"
+    response = requests.get(url)
+    
+    return Image.open(BytesIO(response.content))
+
+def assemble_card_filesystem(card_title,front_img,back_img):
+    directory = f"static/images/{card_title}"
+    os.makedirs(directory, exist_ok=True)
+    front_path = os.path.join(directory,"front.png")
+    back_path = os.path.join(directory,"back.png")
+    front_img.save(front_path)
+    back_img.save(back_path)
+
+def test_filesystem(card_titles):
+    for ct in card_titles:
+        assemble_card_filesystem(ct,get_placeholder(),get_placeholder())
+
+def get_image_paths(card_title,return_which=None):
+    # this probably shouldn't be so hardcoded but idk how to fix that atm
+    if card_title == "":
+        return ""
+    elif return_which == None:
+        return f"images/{card_title}/front.png", f"images/{card_title}/back.png"
+    elif return_which == "back":
+        return f"images/{card_title}/back.png"
+    elif return_which == "front":
+        return f"images/{card_title}/front.png"
+
 def mirror_backs(pagelists):
     pages = []
     for page in pagelists:
@@ -6,7 +40,8 @@ def mirror_backs(pagelists):
 
 def generate_pagelists(cards):
     while len(cards)%9 != 0:
-        cards.append(-1)
-    title_list = [[cards[page*9+card] for card in range(0,9) if cards[page*9+card] != -1] for page in range(0,len(cards)//9)]
-    filenumbers = mirror_backs([[str(page*9+card+1) if cards[page*9+card] != -1 else str(-1) for card in range(0,9) ] for page in range(0,len(cards)//9)])
-    return title_list, filenumbers
+        cards.append("")
+    title_list = [[cards[page*9+card] for card in range(0,9)] for page in range(0,len(cards)//9)]
+    fronts = [[get_image_paths(title,"front") for title in page] for page in title_list]
+    backs = mirror_backs([[get_image_paths(title,"back") for title in page] for page in title_list])
+    return title_list, fronts, backs
