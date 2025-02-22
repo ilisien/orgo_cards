fe60ea685ad26002caadc51dbf7ed555d1820f9f
ilisien <ian8lisien@gmail.com>
2025-02-22 15:46:48 -0500

    example flashcards

--- mol_handling.py
+++ mol_handling.py
@@ -3,6 +3,7 @@ from rdkit.Chem import Draw
 from rdkit.Chem.inchi import MolToInchi, MolFromInchi
 from rdkit.Chem import AllChem
 import pubchempy as pcp
+from PIL import Image
 
 def smiles_to_iupac(smiles,synonyms_amount=3):
     """Convert a SMILES string to an IUPAC name."""
@@ -10,7 +11,8 @@ def smiles_to_iupac(smiles,synonyms_amount=3):
     inchi = MolToInchi(mol)
     compound = pcp.get_compounds(inchi, 'inchi')
     iupac_name = compound[0].iupac_name if compound else "Not found"
-    compound[0].synonyms.remove(iupac_name)
+    if iupac_name in compound[0].synonyms:
+        compound[0].synonyms.remove(iupac_name)
     synonyms = compound[0].synonyms[:synonyms_amount] if compound else "Not found"
     return iupac_name, synonyms
 
@@ -22,17 +24,23 @@ def iupac_to_smiles(iupac_name):
 
 def draw_molecule(smiles):
     mol = Chem.MolFromSmiles(smiles)
-    return Draw.MolToImage(mol)
+    image =  Draw.MolToImage(mol)
+    bbox = image.convert("L").point(lambda x: 0 if x > 250 else 255).getbbox()
+    image.convert("L").point(lambda x: 0 if x > 250 else 255).show()
+    print(bbox)
+    cropped_image = image.crop(bbox)
+    return cropped_image
 
 if __name__ == "__main__":
-    smiles = "CCC=O"
-    print(f"SMILES: {smiles}")
+    #smiles = "CCCO"
+    #print(f"SMILES: {smiles}")
 
-    iupac_name, synonyms = smiles_to_iupac(smiles,5)
-    print(f"IUPAC Name: {iupac_name}")
-    print(f"Synonyms: {synonyms}")
+    #iupac_name, synonyms = smiles_to_iupac(smiles,5)
+    #print(f"IUPAC Name: {iupac_name}")
+    #print(f"Synonyms: {synonyms}")
 
-    new_smiles = iupac_to_smiles(iupac_name)
-    print(f"Reconstructed SMILES: {new_smiles}")
+    #new_smiles = iupac_to_smiles(iupac_name)
+    #print(f"Reconstructed SMILES: {new_smiles}")
 
-    draw_molecule(new_smiles).show()
+    draw_molecule("CCCO").save("1-propanol.png")
+    draw_molecule("CCCCl").save("chloropropane.png")
